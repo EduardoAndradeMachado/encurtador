103eb42c8990b8ae8794ab2a00dda59868e25d62
EduardoAndradeMachado <andrademachadoedu@gmail.com>
2025-06-04 16:30:41 -0300

    feat: adiciona remoção de registros da tabela de logs ao resetar o banco de dados

--- reset_db.py
+++ reset_db.py
@@ -5,7 +5,9 @@ def reset_db():
     cursor = conn.cursor()
     try:
         cursor.execute("DELETE FROM urls;")
+        cursor.execute("DELETE FROM logs;")
         cursor.execute("DELETE FROM sqlite_sequence WHERE name='urls';")
+        cursor.execute("DELETE FROM sqlite_sequence WHERE name='logs';")
         conn.commit()
         print("Banco de dados resetado com sucesso.")
     except Exception as e:
