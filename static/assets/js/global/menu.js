5506ad0c8dd445a7e171c8ded33a3957b554dbbf
EduardoAndradeMachado <andrademachadoedu@gmail.com>
2025-06-05 15:01:01 -0300

    feat: adiciona funcionalidade de destaque para o menu ativo

---
+++ static/assets/js/global/menu.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelectorAll('#menu a');
+    const slug = window.location.pathname;
+    for (let i of menu) {
+        if (slug === i.attributes.href.value) {
+            i.classList.add("menu-active");
+            if (slug == '/resultado'){
+                i.classList.remove('hidden')
+            }
+            break;
+        }
+    }
+});
